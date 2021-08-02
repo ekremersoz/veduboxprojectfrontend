c4c367221940dc2b0c68ef0bd7290348030bb08e
Ekrem Ersöz <ekremersoz06@gmail.com>
2021-08-02 19:36:16 +0300

    comit

--- veduboxfrontend/src/app/modules/WebApi.ts
+++ veduboxfrontend/src/app/modules/WebApi.ts
@@ -1,6 +1,3 @@
 export const apiUrl = "https://webapi20210702164131.azurewebsites.net/api/"
-<<<<<<< HEAD
-export const apiUrl2 = "https://webapi20210702164131.azurewebsites.net/api2/"
-=======
-export const deneme = "";
->>>>>>> 524cad761406e8ae60a55814461f0bfdcf8a1c40
+export const deneme = "deneme";
+
